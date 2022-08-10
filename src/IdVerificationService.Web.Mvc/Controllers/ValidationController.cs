e4d8109a6d36357029a162308bc125d52d3067af
gizempoyraz <64015194+gizempoyraz@users.noreply.github.com>
2022-08-10 15:08:40 +0300

    screen changed

---
+++ src/IdVerificationService.Web.Mvc/Controllers/ValidationController.cs
@@ -0,0 +1,13 @@
+﻿using IdVerificationService.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdVerificationService.Web.Controllers
+{
+    public class ValidationController : IdVerificationServiceControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
